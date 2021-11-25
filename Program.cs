e651e320f1bbec80d1029db57cc5c1866141bfc1
velisalmanli1 <valivs@code.edu.az>
2021-11-26 01:02:47 +0400

    C3 task uploaded

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace C_sharp_first_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 11;
+
+
+            //{ num2 = num1; do { Console.WriteLine(num1 + " " + num2); }
+            //    while (num1 < 6) { num1 = num2; } }
+
+
+
+
+            ////Daha sadə variant budur. Amma hər ikisi işləyir
+            
+            //if (num1 < 6) num1 = num2;
+            //if (num2 > 10) num2 = 5;
+
+            //Console.WriteLine(num1 + " " + num2);
+
+
+        }
+    }
+}
